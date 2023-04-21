88dae643dd416d35a917ff1805acfb7de867a9ab
Israel Tan <israeltan2@gmail.com>
2023-04-21 20:47:51 +0800

    Update for exercises 5.7 and 5.8

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} {blog.url}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  const blogCreator = blog.user ? blog.user.name : null;
+
+  const shownWhenFalse = { display: showDetails ? "none" : "" };
+  const shownWhenTrue = { display: showDetails ? "" : "none" };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      Title: {blog.title}
+      <button onClick={toggleDetails} style={shownWhenFalse}>
+        view details
+      </button>
+      <button onClick={toggleDetails} style={shownWhenTrue}>
+        hide details
+      </button>
+      <div style={shownWhenTrue}>
+        <div>Author: {blog.author}</div>
+        <div>URL: {blog.url}</div>
+        <div>Likes: {blog.likes}</div>
+        <div>User: {blogCreator}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
