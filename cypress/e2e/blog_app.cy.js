365dce415a2f44294adae27dba23d976c2e17899
iltan2 <israeltan2@gmail.com>
2023-04-23 17:02:20 +0800

    Update for exercise 5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,11 +1,36 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "Israel Tan",
+      username: "israel",
+      password: "israel",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
+
     cy.visit("http://localhost:3000");
   });
 
   it("Login form is shown", function () {
-    cy.contains("log in to application")
-    cy.contains("login")
+    cy.contains("log in to application");
+    cy.contains("login");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("israel")
+      cy.get("#password").type("israel")
+      cy.get("#login-button").click()
+
+      cy.contains("Israel Tan logged in")
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("israel")
+      cy.get("#password").type("random")
+      cy.get("#login-button").click()
+
+      cy.contains("Wrong credentials")
+    });
   });
 });
