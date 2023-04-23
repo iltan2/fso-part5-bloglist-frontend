1893518d0220e37f0e08ffb6f01124429c298ebc
iltan2 <israeltan2@gmail.com>
2023-04-23 17:17:25 +0800

    Update for exercise 5.19 and 5.20

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -18,19 +18,63 @@ describe("Blog app", function () {
 
   describe("Login", function () {
     it("succeeds with correct credentials", function () {
-      cy.get("#username").type("israel")
-      cy.get("#password").type("israel")
-      cy.get("#login-button").click()
+      cy.get("#username").type("israel");
+      cy.get("#password").type("israel");
+      cy.get("#login-button").click();
 
-      cy.contains("Israel Tan logged in")
+      cy.contains("Israel Tan logged in");
     });
 
     it("fails with wrong credentials", function () {
-      cy.get("#username").type("israel")
-      cy.get("#password").type("random")
-      cy.get("#login-button").click()
+      cy.get("#username").type("israel");
+      cy.get("#password").type("random");
+      cy.get("#login-button").click();
 
-      cy.contains("Wrong credentials")
+      cy.contains("Wrong credentials");
     });
+  });
+
+  describe("When logged in", function () {
+    beforeEach(function () {
+      cy.request("POST", "http://localhost:3003/api/login", {
+        username: "israel",
+        password: "israel",
+      }).then((response) => {
+        localStorage.setItem(
+          "loggedBlogAppUser",
+          JSON.stringify(response.body)
+        );
+        cy.visit("http://localhost:3000");
+      });
+    });
+
+    it("A blog can be created", function () {
+      cy.contains("new blog").click();
+      cy.get("input[placeholder='Input title here']").type(
+        "a blog created by cypress"
+      );
+      cy.get("input[placeholder='Input author here']").type("Cypress");
+      cy.get("input[placeholder='Input URL here']").type("http://sampleurl");
+      cy.contains("add to blogs").click();
+
+      cy.contains("a blog created by cypress");
+    });
+
+    it("User can like a blog", function () {
+      cy.contains("new blog").click();
+      cy.get("input[placeholder='Input title here']").type(
+        "a blog created by cypress"
+      );
+      cy.get("input[placeholder='Input author here']").type("Cypress");
+      cy.get("input[placeholder='Input URL here']").type("http://sampleurl");
+      cy.contains("add to blogs").click();
+
+      cy.contains("view details").click();
+      cy.contains("add like").click()
+
+      cy.contains("Likes: 1")
+
+    })
+
   });
 });
